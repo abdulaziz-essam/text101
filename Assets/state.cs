63c372f49ccb6d7235f986b67d8e499c226d6fbf
aek20 <66400022+aek20@users.noreply.github.com>
2021-06-20 03:19:02 -0700

    unity project

---
+++ Assets/state.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "state")];
+public class state : ScriptableObject
+{
+    [SerializeField]string storyText;
+}
